db195d6011261b629fe44d6ca523deeabea0cb89
David Ko <davidsj.ko@gmail.com>
2023-04-10 09:48:31 -0400

    ultra-basic http server

--- devops/main.go
+++ devops/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	http.HandleFunc("/", func(r http.ResponseWriter, req *http.Request) {
+		fmt.Println(fmt.Sprintf("%s %s", req.Method, req.URL.Path))
+		r.Write([]byte("Hello world!"))
+	})
+
+	fmt.Println("Listening on 0.0.0.0:3000...")
+	http.ListenAndServe(":3000", nil)
 }
